0255c234898d635a8ca20fe3f402b6064da53c29
Wenkai Yin <yinw@vmware.com>
2017-06-21 18:19:22 +0800

    update

--- frontend.sh
+++ frontend.sh
@@ -3,9 +3,9 @@ set -e
 
 source common.sh
 
-clarity_image=vmware/harbor-clarity-ui-builder:1.1.0
+clarity_image=vmware/harbor-clarity-ui-builder:1.1.2
 static=$harbor/src/ui/static
 ui_ng=$harbor/src/ui_ng/src
 
-docker run --rm -v $static:/clarity-seed/dist -v $ui_ng:/clarity-seed/src \
+docker run --rm -v $static:/harbor_ui/dist -v $ui_ng:/harbor_ui/src \
 $clarity_image /bin/bash /entrypoint.sh
